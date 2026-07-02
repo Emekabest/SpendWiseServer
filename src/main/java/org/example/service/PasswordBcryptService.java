2a93027dc72f069a94cf8f288f9c489da9788b29
EmekaBest <josephemeka2611@gmail.com>
2026-07-02 02:26:09 +0100

    Added security configuration feature--2

--- src/main/java/org/example/service/PasswordBcryptService.java
+++ src/main/java/org/example/service/PasswordBcryptService.java
@@ -2,10 +2,8 @@ package org.example.service;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class PasswordBcryptService {
@@ -17,14 +15,14 @@ public class PasswordBcryptService {
     }
 
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable()) // disable CSRF for simplicity (Lambda REST API doesn’t need it)
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // allow all requests without auth
-                );
-        return http.build();
-    }
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(csrf -> csrf.disable()) // disable CSRF for simplicity (Lambda REST API doesn’t need it)
+//                .authorizeHttpRequests(auth -> auth
+//                        .anyRequest().permitAll() // allow all requests without auth
+//                );
+//        return http.build();
+//    }
 
 }
