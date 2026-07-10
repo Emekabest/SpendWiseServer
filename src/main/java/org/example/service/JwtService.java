01f99e327eb6ac6508aa00ef3e95e1ef24b21a38
EmekaBest <josephemeka2611@gmail.com>
2026-07-11 00:29:28 +0100

    Working on Spring Application Event--2

--- src/main/java/org/example/service/JwtService.java
+++ src/main/java/org/example/service/JwtService.java
@@ -19,7 +19,7 @@ public class JwtService {
     @Value("${jwt.secret}")
     private String secret;
 
-    private static final long ACCESSTOKEN_JWTEXPIRATION = 1000 * 60 * 10;//
+    private static final long ACCESSTOKEN_JWTEXPIRATION = 1000 * 60;//
     private static final long REFRESHTOKEN_JWTEXPIRATION = 1000 * 60 * 60;
 
 
